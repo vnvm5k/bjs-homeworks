7c3dd957fcde78201fcb0f88c9145f6ac03988de
Vladimir Vorobtsov <vnvm5k@yandex.ru>
2020-10-07 18:35:17 +0300

    HomeTask

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,28 @@
+'Use strict'; 
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    if ((percent || contribution || amount || date) !== 'number' && (percent || contribution || amount || date) !== 'string') {
+    	return "Параметр содержит неправильное значение"    	
+    }else {
+    	parseInt(percent,contribution,amount,date);
+    	let sum = amount - contribution;
+    	let percentPart = 1/12 * (percent/100);  
+    	
+		let current_date = new Date();
+		let difference = (date.getFullYear()*12 + date.getMonth()) - (current_date.getFullYear()*12 + current_date.getMonth());	
+		let monthPay = sum * (percentPart + (percentPart/(((1+percentPart)**difference)-1)));
+		let totalAmount = difference * monthPay;
+		console.log(totalAmount.toFixed(2));
+    }
+    return totalAmount.toFixed(2);
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting; 
+    if (name === 'null' || name === 'undefined' || name === '') {
+    	greeting = 'Привет, мир! Меня зовут Аноним';
+    } else {
+    	greeting = `Привет, мир! Меня зовут ${name}`;
+    }
+    return greeting;
 }
