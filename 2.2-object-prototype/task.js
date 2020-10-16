1d0dbc03ebcb6d48e1052f948aa229fc1f2f657c
Vladimir Vorobtsov <vnvm5k@yandex.ru>
2020-10-16 22:06:14 +0300

    Update

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,36 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function () {
+	
+	let init = this.split(' ').join('').toLowerCase(); 
+	let rev = init.split('').reverse().join(''); 
+
+	return (rev === init) ? true : false; 
+} 
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    
+    let averageMark = 0;
+    let roundedAverage = 0;
+
+    if (marks.length == 0) {
+    return 0;
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+    averageMark += marks[i];  
+    }
+
+    roundedAverage = Math.round(averageMark/marks.length);
+    return roundedAverage;
+
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    
+    let diff = Date.now() - +(new Date(birthday)); 
+    let age = Math.floor(diff / 31536000000);
+
+    return (age > 18) ? '> 18 лет' : false; 
+    
 }
